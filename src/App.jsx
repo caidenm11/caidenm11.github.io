bf87260a4987c31117dd6b37282f36d06912788d
Caiden <caiden.merklin@gmail.com>
2024-11-03 14:09:05 -0800

    added files from the lab4 repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar.jsx";
+import React from "react";
+
+const App  = () => {
+  return(
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,26 @@
+import logo from "../assets/just-cm-svg.svg"
+import {FaLinkedin} from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
+
+// TODO -> change this to my logo
+// TODO -> follow my mockup with the hyperlinks
+
+const Navbar = () => {
+  return ( 
+        <nav className="bg-red-700 mb-20 flex items-center justify-between py-3" >
+            <div className="flex flex-shrink-0 items-center">
+                <img src={logo} 
+
+                alt="logo" />
+            </div>
+            <div className = "m-8 flex items-center justify-center gap-4">
+                <FaGithub>
+                </FaGithub>
+                <FaLinkedin></FaLinkedin>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar

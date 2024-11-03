86fa15b979dac4fc7f808d4bc30fe705c08addc2
Caiden <caiden.merklin@gmail.com>
2024-11-03 14:07:42 -0800

    added files from the lab4 repo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ import {FaGithub} from "react-icons/fa";
 const Navbar = () => {
   return ( 
         <nav className="bg-red-700 mb-20 flex items-center justify-between py-3" >
-            <div className="flex flex-shrink-0 items-center">
+            <div className="flex flex-shrink- items-center">
                 <img src={logo} 
 
                 alt="logo" />

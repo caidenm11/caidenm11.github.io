e91e100dc3dec1e1c645636dfe97ed74419f0f03
Caiden <caiden.merklin@gmail.com>
2024-11-03 21:45:09 -0800

    added the background image to the front page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,14 @@
+import backgroundLocation from './assets/background-image.webp';
 const Home = () => {
     return (
-      <section id="home" className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div style={{backgroundImage: `url(${backgroundLocation})`}}>
+      <section id="home" className="min-h-screen flex items-center justify-center bg-gray-100 bg-cover bg-center"
+      style={{ backgroundImage: `url(${backgroundLocation})` }}>
+       
         <h1 className="text-4xl font-bold">Welcome to My Website</h1>
+        {/* Make a line here */}
       </section>
+      </div>
     );
   };
   
